919434728c396f04c856fbb20c92a70b348f4964
Peter Thomas Horn <7641832+peterthomashorn@users.noreply.github.com>
2019-10-28 22:06:41 +0100

    Initial Commit

---
+++ WKWebViewNotificationExample/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  WKWebViewNotificationExample
+//
+//  Created by Private on 28.10.19.
+//  Copyright © 2019 Peter Thomas Horn. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ WKWebViewNotificationExample/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WKWebViewNotificationExample
+//
+//  Created by Private on 28.10.19.
+//  Copyright © 2019 Peter Thomas Horn. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
