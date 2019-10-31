642c0f820ff44fc85b1a3e3c47d626bd44dca0b1
Peter Thomas Horn <7641832+peterthomashorn@users.noreply.github.com>
2019-10-31 12:21:32 +0100

    Extended example user script to cover more of the basic Notification API.

--- WKWebViewNotificationExample/UserScript.js
+++ WKWebViewNotificationExample/UserScript.js
@@ -1,3 +1,23 @@
-window.Notification = function (messageText) {
-    window.webkit.messageHandlers.notify.postMessage(messageText);
-};
+/**
+ * Incomplete Notification API override to enable native notifications.
+ */
+class NotificationOverride {
+    // Grant permission by default to keep this example simple.
+    // Safari 13 does not support class fields yet, so a static getter must be used.
+    static get permission() {
+        return "granted";
+    }
+    
+    // Safari 13 still uses callbacks instead of promises.
+    static requestPermission (callback) {
+        callback("granted");
+    }
+    
+    // Forward the notifiation text to the native app through the script message handler.
+    constructor (messageText) {
+        window.webkit.messageHandlers.notify.postMessage(messageText);
+    }
+}
+
+// Override the global browser notification object.
+window.Notification = NotificationOverride;
